3ec66e92ab46e56e7875e0c99e6d807b13ff4b36
Sudarshan jhawar <sudarshan.jhawar@oitcolitcaz.com>
2018-05-03 09:25:28 +0530

    product-description : getinfo method correction

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -13,6 +13,6 @@ export class ProductDescriptionComponent implements OnInit {
   ngOnInit() {
     this._productService
       .getAlbum(1)
-      .subscribe(response => (this.albumInfo = response));
+      .subscribe(response => this.albumInfo = response );
   }
 }
