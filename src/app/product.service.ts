3ffd4e3f28ec569c9dec9433bac5f50388185aff
Sudarshan jhawar <sudarshan.jhawar@oitcolitcaz.com>
2018-05-07 18:21:55 +0530

    interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,3 +1,5 @@
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
@@ -6,7 +8,7 @@ import 'rxjs/add/operator/map';
 export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) {}
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum (id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 }
